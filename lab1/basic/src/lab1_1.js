56f2fb5d72cf0e3d819922a7f71431ecbb42bef3
Your Name <you@example.com>
2024-02-20 18:06:19 +0300

    fundamental 1

---
+++ lab1/basic/src/lab1_1.js
@@ -0,0 +1,65 @@
+function getMonth(number) {
+  number = Number(number)
+  if (isNaN(number) || number < 1 || number > 12) {
+    return 'Wrong number'
+  } 
+
+  switch(number) {
+    case 1: return "Jan"
+    case 2: return "Feb"
+    case 3: return "Mar"
+    case 4: return "Apr"
+    case 5: return "May"
+    case 6: return "Jun"
+    case 7: return "Jul"
+    case 8: return "Aug"
+    case 9: return "Sep"
+    case 10: return "Oct"
+    case 11: return "Nov"
+    case 12: return "Dec"
+  }
+}
+
+function getPrimes(number) {
+  number = Number(number)
+  if (isNaN(number) || number < 0) {
+    return 'Wrong number'
+  }
+
+  let primes = []
+  while (primes.length < number) {
+    primes.push(2)
+  }
+
+  return primes.join(' ')
+}
+
+let Counter = {
+  count: 0,
+  add: function(num) {
+    num = Number(num)
+    if (isNaN(num)) {
+      throw new Error("wrong number")
+    }
+    this.count += num
+  },
+
+  sub: function(num) {
+    num = Number(num)
+    if (isNaN(num)) {
+      throw new Error("wrong number")
+    }
+    this.count -= num
+  },
+}
+
+function changeDelimeter(str) {
+  console.log(str)
+  return str.split(',').join('.')
+}
+
+function checkPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+
+  return reversed == str ? "Да" : "Нет"
+}
