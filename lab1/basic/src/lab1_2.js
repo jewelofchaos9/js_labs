5fdd778bac06261efd57e5ebaa7f4c48f66fce6c
Your Name <you@example.com>
2024-02-21 14:35:52 +0300

    fundamental 2

--- lab1/basic/src/lab1_2.js
+++ lab1/basic/src/lab1_2.js
@@ -31,11 +31,60 @@ function findResidues(arr) {
 function findMedian(...args) {
   let sum = 0
   for(let i = 0; i < args.length; i++) {
+    console.log(args[i])
     if (isNaN(args[i])) {
-      throw new Error("NaN if find median")
+      throw new Error("NaN in find median")
     }
-    sum += args[i] 
+    sum += Number(args[i])
   }
+  console.log(sum)
+  console.log(args.length)
 
   return sum / args.length
+}
+
+function checkBrackets(bracketsString) {
+  let stack = []
+  let brackets = "()"
+  let state = true
+  bracketsString.split("").forEach(function(elem, index, array) {
+    if (!brackets.includes(elem)) {
+      state = false
+      return
+    }
+
+    if (elem == "(") {
+      stack.push(")")
+    }
+
+    else {
+      if (stack.pop() != elem) {
+        state = false
+        return 
+      }
+    }
+  })
+
+  return stack.length == 0 && state ? "Правильная" : "Неправильная"
+}
+
+function isObject(value) {
+  return typeof value == "object" && value != null && !Array.isArray(value)
+}
+
+function cloneObject(obj) {
+  let clone = {}
+  for (let prop in obj) {
+    if (isObject(obj[prop])) {
+      clone[prop] = cloneObject(obj[prop])
+    }
+    else if(Array.isArray(obj[prop])) {
+      clone[prop] = [...obj[prop]]
+    }
+    else {
+      clone[prop] = obj[prop]
+    }
+  }
+
+  return clone
 }
